3f9faed75d0e7fd69788b6e82c130e390cc35f33
Zvi Grinberg <zgrinber@redhat.com>
2024-03-14 12:48:47 +0200

    add action.yml and script that performs the logic
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

---
+++ is-retest-needed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HEAD_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "The following changes since commit" | awk '{print $6}' | awk -F : '{print $1}')
+BASE_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "for you to fetch changes up to" | awk '{print $8}' | awk -F : '{print $1}')
+if [[ "${HEAD_REF}" == "${BASE_REF}" ]]; then
+      echo "result=false" >> "$GITHUB_OUTPUT"
+  else
+    echo "result=true" >> "$GITHUB_OUTPUT"
+fi
