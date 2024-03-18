559d293cbc959ad2d4f08a548cdffe2cd19d4a86
Zvi Grinberg <zgrinber@redhat.com>
2024-03-19 01:51:53 +0200

    add more debug printings
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- is-retest-needed.sh
+++ is-retest-needed.sh
@@ -4,6 +4,7 @@ echo "current directory:"
 echo $(pwd)
 echo "HEAD_BRANCH=$HEAD_BRANCH"
 echo "$BASE_BRANCH=$BASE_BRANCH"
+git branch -av
 git request-pull "$HEAD_BRANCH" ./ "$BASE_BRANCH"
 HEAD_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "The following changes since commit" | awk '{print $6}' | awk -F : '{print $1}')
 BASE_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "for you to fetch changes up to" | awk '{print $8}' | awk -F : '{print $1}')
