f92af1dec54e6e57e5a109824f65b7e59f6e4ea7
Zvi Grinberg <zgrinber@redhat.com>
2024-03-18 23:18:31 +0200

    fix: script should know where git repo is in workspace
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- is-retest-needed.sh
+++ is-retest-needed.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd $GITHUB_REPOSITORY
 HEAD_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "The following changes since commit" | awk '{print $6}' | awk -F : '{print $1}')
 BASE_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "for you to fetch changes up to" | awk '{print $8}' | awk -F : '{print $1}')
 if [[ "${HEAD_REF}" == "${BASE_REF}" ]]; then
