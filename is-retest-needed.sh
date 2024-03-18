dfe41ceba514829e6d84d3641d7ea2bd34042763
Zvi Grinberg <zgrinber@redhat.com>
2024-03-18 23:31:37 +0200

    fix: restore git repo work dir
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- is-retest-needed.sh
+++ is-retest-needed.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd $GITHUB_REPOSITORY
+echo "current directory:"
+echo $(pwd)
 HEAD_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "The following changes since commit" | awk '{print $6}' | awk -F : '{print $1}')
 BASE_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "for you to fetch changes up to" | awk '{print $8}' | awk -F : '{print $1}')
 if [[ "${HEAD_REF}" == "${BASE_REF}" ]]; then
