7b8979da5ec8c2125e84aae19c1ddba57bd0fe30
Zvi Grinberg <zgrinber@redhat.com>
2024-03-19 02:42:46 +0200

    fix: limit greping
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- is-retest-needed.sh
+++ is-retest-needed.sh
@@ -4,9 +4,9 @@ echo "current directory:"
 echo $(pwd)
 echo "HEAD_BRANCH=$HEAD_BRANCH"
 echo "BASE_BRANCH=$BASE_BRANCH"
-CHECKED_OUT_REF= $(git rev-parse  --abbrev-ref HEAD)
+CHECKED_OUT_REF=$(git rev-parse  --abbrev-ref HEAD)
 # Save original checked out branch name
-HEAD_BRANCH_REMOTE=$(git branch -av | grep $HEAD_BRANCH | awk '{print $1}')
+HEAD_BRANCH_REMOTE=$(git branch -av | grep $HEAD_BRANCH -m 1| awk '{print $1}')
 git checkout $HEAD_BRANCH_REMOTE
 git branch $HEAD_BRANCH
 git request-pull "$HEAD_BRANCH" ./ "$BASE_BRANCH"
