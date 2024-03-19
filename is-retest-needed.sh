80bda5a90824cbd2d53081e8ec0b842365d5128a
Zvi Grinberg <zgrinber@redhat.com>
2024-03-19 02:50:40 +0200

    fix: fix parsing git branch -av
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- is-retest-needed.sh
+++ is-retest-needed.sh
@@ -6,7 +6,7 @@ echo "HEAD_BRANCH=$HEAD_BRANCH"
 echo "BASE_BRANCH=$BASE_BRANCH"
 CHECKED_OUT_REF=$(git rev-parse  --abbrev-ref HEAD)
 # Save original checked out branch name
-HEAD_BRANCH_REMOTE=$(git branch -av | grep $HEAD_BRANCH -m 1| awk '{print $1}')
+HEAD_BRANCH_REMOTE=$(git branch -av | grep -v "*" |grep $HEAD_BRANCH -m 1| awk '{print $1}')
 git branch -av
 echo "HEAD_BRANCH_REMOTE=$HEAD_BRANCH_REMOTE"
 git checkout $HEAD_BRANCH_REMOTE
