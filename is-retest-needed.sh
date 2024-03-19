85e682f459687f4cf9ec17542f67b47ded981ae1
Zvi Grinberg <zgrinber@redhat.com>
2024-03-19 02:26:16 +0200

    fix: split checkout and branch creation into 2 commands
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- is-retest-needed.sh
+++ is-retest-needed.sh
@@ -4,13 +4,18 @@ echo "current directory:"
 echo $(pwd)
 echo "HEAD_BRANCH=$HEAD_BRANCH"
 echo "BASE_BRANCH=$BASE_BRANCH"
+CHECKED_OUT_REF= $(git rev-parse  --abbrev-ref HEAD)
+# Save original checked out branch name
 HEAD_BRANCH_REMOTE=$(git branch -av | grep $HEAD_BRANCH | awk '{print $1}')
-git checkout $HEAD_BRANCH_REMOTE -b $HEAD_BRANCH
+git checkout $HEAD_BRANCH_REMOTE
+git branch $HEAD_BRANCH
 git request-pull "$HEAD_BRANCH" ./ "$BASE_BRANCH"
 HEAD_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "The following changes since commit" | awk '{print $6}' | awk -F : '{print $1}')
 BASE_REF=$(git request-pull -p "$HEAD_BRANCH" ./ "$BASE_BRANCH" | grep "for you to fetch changes up to" | awk '{print $8}' | awk -F : '{print $1}')
 echo "HEAD_REF= $HEAD_REF"
 echo "BASE_REF= $BASE_REF"
+# check out back to original branch name
+git checkout $CHECKED_OUT_REF
 if [[ "${HEAD_REF}" == "${BASE_REF}" ]]; then
       echo "result=false" >> "$GITHUB_OUTPUT"
   else
